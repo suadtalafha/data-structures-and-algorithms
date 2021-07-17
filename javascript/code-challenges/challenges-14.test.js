fa0099b9788fb87343438e22e640519c04479309
suadtalafha <suadhusam@yahoo.com>
2021-07-17 14:07:37 +0300

    save

--- javascript/code-challenges/challenges-14.test.js
+++ javascript/code-challenges/challenges-14.test.js
@@ -196,10 +196,19 @@ Here is a sample board:
   ['X', 'O', 'X'],
 ];
 ------------------------------------------------------------------------------------------------ */
-
 const detectTicTacToeWin = (board) => {
   // Solution code here...
-};
+let x = board[0];
+let y = board[1];
+let z = board[2];
+for (let i = 0; i < board.length; i++) {
+  if (x[0] == y[0] == z[0] || x[0] == y[1] == z[2] || x[2] == y[1] == z[0] || x[0] == x[1] == x[2] || y[0] == y[1] == y[2] || z[0] == z[1] == z[2] || x[1] == y[1] == z[1] || x[2] == y[2] == z[2]) {
+    return true
+  } else {
+    return false
+  }
+}
+ return board; 
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
